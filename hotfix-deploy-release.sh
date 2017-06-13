f27353dc4fe3f50dab7999aace883e5ae3778888
Gregory Boddin <gregory@siwhine.net>
2017-06-13 12:15:43 +0200

    Small helper tool to apply hotfixes on releases

--- hotfix-deploy-release.sh
+++ hotfix-deploy-release.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Die if issue :
-set -e
+set -ex
 # get commit from cli :
 commit=$1
 # Shift $1 out of the stack
 shift
 # Patch frenzy :
+git fetch
 for to_patch in $@; do
+        git fetch origin ${to_patch}
 	git checkout ${to_patch} 
-	git cherry-pick ${commit} # merge patch
+	git cherry-pick -X theirs -x ${commit} # merge patch
 	git push origin :${to_patch} # delete remote tag
 	git tag -d ${to_patch} # delete local tag
 	git tag ${to_patch} # local tag
